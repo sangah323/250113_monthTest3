3e3064fccb36e783b0ee17b28c808a6b0746b64c
sangah323 <choa323@naver.com>
2025-01-13 15:12:05 +0900

    feat : Delete 기능 구현

--- routes/comment.route.js
+++ routes/comment.route.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const {
   getCommentList,
   postCommentWrite,
-} = require("../controllers/comment.controller.js");
+  postCommentDelete,
+} = require("../controllers/comment.controller");
 
 router.get("/list", getCommentList);
 
 router.post("/write", postCommentWrite);
+
+router.post("/delete/:id", postCommentDelete);
 
 module.exports = router;
