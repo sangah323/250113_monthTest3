36520baff85c4af060dac7d5911a8616d32475d2
sangah323 <choa323@naver.com>
2025-01-13 14:08:01 +0900

    feat : create 기능 구현 / 수정 필요(undefined)

--- routes/comment.route.js
+++ routes/comment.route.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getCommentList } = require("../controllers/comment.controller.js");
+const {
+  getCommentList,
+  postCommentWrite,
+} = require("../controllers/comment.controller.js");
 
 router.get("/list", getCommentList);
+
+router.post("/write", postCommentWrite);
 
 module.exports = router;
